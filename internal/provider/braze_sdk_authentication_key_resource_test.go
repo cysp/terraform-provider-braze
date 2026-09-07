d4582e303bdfa8cbbef6a310493f64fc0296a98d
Scott Talbot <s@chikachow.org>
2026-09-08 08:11:06 +1000

    test: cover SDK key promotion in a single lifecycle

--- internal/provider/braze_sdk_authentication_key_resource_test.go
+++ internal/provider/braze_sdk_authentication_key_resource_test.go
@@ -9,8 +9,13 @@ import (
 
 	brazeclient "github.com/cysp/terraform-provider-braze/internal/braze-client-go"
 	brazeclienttesting "github.com/cysp/terraform-provider-braze/internal/braze-client-go/testing"
+	"github.com/hashicorp/terraform-plugin-testing/compare"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -39,81 +44,100 @@ resource "braze_sdk_authentication_key" "test" {
 func TestAccBrazeSDKAuthenticationKey(t *testing.T) {
 	t.Parallel()
 
-	server, _ := brazeclienttesting.NewBrazeServer()
+	const (
+		appID   = "01234567-89ab-cdef-0123-456789abcdef"
+		address = "braze_sdk_authentication_key.test"
+	)
 
-	BrazeProviderMockedResourceTest(t, server, resource.TestCase{
+	server, err := brazeclienttesting.NewBrazeServer()
+	require.NoError(t, err)
+
+	external := brazeclient.SDKAuthenticationKey{ID: "external", RsaPublicKey: "external public key", Description: "External key", IsPrimary: true}
+	server.SetSDKAuthenticationKey(appID, external)
+
+	var creates, promotions, deletions atomic.Int32
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		switch req.Method {
+		case http.MethodPost:
+			creates.Add(1)
+		case http.MethodPut:
+			promotions.Add(1)
+		case http.MethodDelete:
+			deletions.Add(1)
+		}
+
+		server.ServeHTTP(w, req)
+	})
+	id := statecheck.CompareValue(compare.ValuesSame())
+	stateChecks := func(primary bool) []statecheck.StateCheck {
+		return []statecheck.StateCheck{
+			statecheck.ExpectKnownValue(address, tfjsonpath.New("id"), knownvalue.NotNull()),
+			id.AddStateValue(address, tfjsonpath.New("id")),
+			statecheck.ExpectKnownValue(address, tfjsonpath.New("app_id"), knownvalue.StringExact(appID)),
+			statecheck.ExpectKnownValue(address, tfjsonpath.New("primary"), knownvalue.Bool(primary)),
+		}
+	}
+
+	var managedID string
+
+	assertRemote := func(primary bool, expectedPromotions int32) {
+		t.Helper()
+
+		response, err := server.Handler().ListSDKAuthenticationKeys(t.Context(), brazeclient.ListSDKAuthenticationKeysParams{AppID: appID})
+		require.NoError(t, err)
+		require.Len(t, response.Response.Keys, 2)
+
+		for _, key := range response.Response.Keys {
+			if key.ID == external.ID {
+				assert.Equal(t, !primary, key.IsPrimary)
+				assert.Equal(t, external.RsaPublicKey, key.RsaPublicKey)
+
+				continue
+			}
+
+			if managedID == "" {
+				managedID = key.ID
+			}
+
+			assert.Equal(t, managedID, key.ID, "promotion and import must preserve the existing key")
+			assert.Equal(t, primary, key.IsPrimary)
+			assert.Equal(t, "Terraform-managed SDK Authentication key", key.Description)
+		}
+
+		assert.EqualValues(t, 1, creates.Load())
+		assert.Equal(t, expectedPromotions, promotions.Load())
+		assert.Zero(t, deletions.Load())
+	}
+	promoteExternal := func() {
+		_, err := server.Handler().SetPrimarySDKAuthenticationKey(t.Context(), &brazeclient.SetPrimarySDKAuthenticationKeyRequest{AppID: appID, KeyID: external.ID})
+		require.NoError(t, err)
+	}
+	primaryConfig := testSDKAuthenticationKeyPrimaryConfig("Terraform-managed SDK Authentication key")
+	update := resource.ConfigPlanChecks{PreApply: []plancheck.PlanCheck{plancheck.ExpectResourceAction(address, plancheck.ResourceActionUpdate)}}
+
+	BrazeProviderMockedResourceTest(t, handler, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{tfversion.SkipBelow(tfversion.Version1_12_0)},
 		Steps: []resource.TestStep{
+			{Config: testSDKAuthenticationKeyConfig, ConfigStateChecks: stateChecks(false), PostApplyFunc: func() { assertRemote(false, 0) }},
+			{Config: testSDKAuthenticationKeyConfig, ResourceName: address, ImportState: true, ImportStateIdPrefix: appID + "/", ImportStateVerify: true},
+			{Config: testSDKAuthenticationKeyConfig, ResourceName: address, ImportState: true, ImportStateKind: resource.ImportBlockWithResourceIdentity},
+			{Config: primaryConfig, ConfigPlanChecks: update, ConfigStateChecks: stateChecks(true), PostApplyFunc: func() { assertRemote(true, 1) }},
+			{PreConfig: promoteExternal, Config: primaryConfig, ConfigPlanChecks: update, ConfigStateChecks: stateChecks(true), PostApplyFunc: func() { assertRemote(true, 2) }},
+			// Relinquish the primary claim before destroy; the managed secondary
+			// must be deleted while the external primary remains untouched.
 			{
-				Config: testSDKAuthenticationKeyConfig,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("braze_sdk_authentication_key.test", "id"),
-					resource.TestCheckResourceAttr(
-						"braze_sdk_authentication_key.test",
-						"app_id",
-						"01234567-89ab-cdef-0123-456789abcdef",
-					),
-					resource.TestCheckResourceAttr(
-						"braze_sdk_authentication_key.test",
-						"rsa_public_key",
-						"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAknOke8rjbXy2LD6DVEGu\nw8V6k+VsRINnGROwXFlrIXVUQ1NY4Dd6vb+0kydgwrH9/zB5nJgRuFkXUH1Iigbl\nCI/Bo9m7JVMqICRpvuKzCCzTn3qzgPSE+7TDwQJfLomo12DDxqcykIR11Y0Nx6mJ\nnOZnCEDgppNtZpdnNOzwE8WGKyNd/JI613mekBByrkmc3boGzAESxoBLAMwQIRgp\nk6+XJ5i/dPWUbk33Lt8QjFH+aZ+0hLKx0IcPGYKFsF87ZQ7b8dpARu/D5i/VhV5n\n7Q7wvzZwt9NMQ8SLzSXGrE7H3wf8/ag7TySmMsANLYIMCsopTXcHdaqJe3QRyPUH\nLQIDAQAB\n-----END PUBLIC KEY-----\n",
-					),
-					resource.TestCheckResourceAttr(
-						"braze_sdk_authentication_key.test",
-						"description",
-						"Terraform-managed SDK Authentication key",
-					),
-					resource.TestCheckResourceAttrSet("braze_sdk_authentication_key.test", "primary"),
-				),
-			},
-			{
-				Config: testSDKAuthenticationKeyConfig,
+				PreConfig: promoteExternal, Config: testSDKAuthenticationKeyConfig,
+				ConfigPlanChecks:  resource.ConfigPlanChecks{PreApply: []plancheck.PlanCheck{plancheck.ExpectEmptyPlan()}},
+				ConfigStateChecks: stateChecks(false), PostApplyFunc: func() { assertRemote(false, 2) },
 			},
 		},
 	})
-}
 
-func TestAccBrazeSDKAuthenticationKeyImport(t *testing.T) {
-	t.Parallel()
-
-	server, _ := brazeclienttesting.NewBrazeServer()
-
-	BrazeProviderMockedResourceTest(t, server, resource.TestCase{
-		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
-			tfversion.SkipBelow(tfversion.Version1_12_0),
-		},
-		Steps: []resource.TestStep{
-			{
-				Config: testSDKAuthenticationKeyConfig,
-			},
-			{
-				Config:          testSDKAuthenticationKeyConfig,
-				ResourceName:    "braze_sdk_authentication_key.test",
-				ImportState:     true,
-				ImportStateKind: resource.ImportBlockWithResourceIdentity,
-			},
-		},
-	})
-}
-
-func TestAccBrazeSDKAuthenticationKeyCompositeImport(t *testing.T) {
-	t.Parallel()
-
-	server, _ := brazeclienttesting.NewBrazeServer()
-
-	BrazeProviderMockedResourceTest(t, server, resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				Config: testSDKAuthenticationKeyConfig,
-			},
-			{
-				Config:              testSDKAuthenticationKeyConfig,
-				ResourceName:        "braze_sdk_authentication_key.test",
-				ImportState:         true,
-				ImportStateIdPrefix: "01234567-89ab-cdef-0123-456789abcdef/",
-				ImportStateVerify:   true,
-			},
-		},
-	})
+	response, err := server.Handler().ListSDKAuthenticationKeys(t.Context(), brazeclient.ListSDKAuthenticationKeysParams{AppID: appID})
+	require.NoError(t, err)
+	assert.Equal(t, []brazeclient.SDKAuthenticationKey{external}, response.Response.Keys)
+	assert.EqualValues(t, 1, deletions.Load())
 }
 
 func TestAccBrazeSDKAuthenticationKeyRejectsPrimaryDestroy(t *testing.T) {
