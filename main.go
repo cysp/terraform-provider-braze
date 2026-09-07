92529c419e5c9e0a20315f62d4ef65ab49f8d5a4
Scott Talbot <s@chikachow.org>
2026-09-08 08:09:48 +1000

    feat(cli): report provider version and source commit

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/cysp/terraform-provider-braze/internal/provider"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -19,11 +21,27 @@ import (
 // set by goreleaser.
 var version = "dev"
 
+//nolint:gochecknoglobals // Set by GoReleaser.
+var commit = "unknown"
+
 func main() {
-	var debug bool
+	var (
+		debug       bool
+		showVersion bool
+	)
 
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.BoolVar(&showVersion, "version", false, "print the provider version and exit")
 	flag.Parse()
+
+	if showVersion {
+		_, err := fmt.Fprintf(os.Stdout, "terraform-provider-braze %s (commit %s)\n", version, commit)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		return
+	}
 
 	opts := providerserver.ServeOpts{
 		Address: "registry.terraform.io/cysp/braze",
